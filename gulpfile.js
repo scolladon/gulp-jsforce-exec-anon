51e4289bd8a8ae61f52aafdf0dbd731fbfd108c7
Sébastien Colladon <scolladon@salesforce.com>
2017-03-16 22:55:00 +0100

    Fix include

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,9 @@
 'use strict';
 var fs = require('fs');
 var gulp = require('gulp');
-var zip = require('gulp-zip');
 var execAnon = require('./');
 
-gulp.task('execanon', function() {
+gulp.task('exec-anon', function() {
   gulp.src("./script/*")
     .pipe(execAnon({
       username: process.env.SF_USERNAME,
