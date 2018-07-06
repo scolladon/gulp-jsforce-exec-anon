0e60b093dd80ace38864a4f56fc75f0dd0c8a630
scolladon <colladonsebastien@gmail.com>
2018-07-06 11:10:08 +0200

    Typo fix

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const through = require('through2');
 const jsforce = require('jsforce');
 const path = require('path');
 
-const PULGIN_NAME = 'gulp-jsforce-exec-anon';
+const PLUGIN_NAME = 'gulp-jsforce-exec-anon';
 
 module.exports = options =>{
   return through.obj((file, enc, callback) => {
@@ -15,11 +15,11 @@ module.exports = options =>{
 
 
     conn.login(options.username, options.password, (error, userInfo) => {
-      if (error) { return callback(new gutil.PluginError(PULGIN_NAME,error)); }
+      if (error) { return callback(new gutil.PluginError(PLUGIN_NAME,error)); }
       conn.tooling.executeAnonymous(file.contents, (err, res) => {
-        if (err) { return callback(new gutil.PluginError(PULGIN_NAME,err)); }
-        else if (!res.success) { return callback(new gutil.PluginError(PULGIN_NAME,res.compileProblem)); }
-        gutil.log(PULGIN_NAME, path.basename(file.path) + ' successfuly executed anonymously ', gutil.colors.green(':)'))
+        if (err) { return callback(new gutil.PluginError(PLUGIN_NAME,err)); }
+        else if (!res.success) { return callback(new gutil.PluginError(PLUGIN_NAME,res.compileProblem)); }
+        gutil.log(PLUGIN_NAME, path.basename(file.path) + ' successfuly executed anonymously ', gutil.colors.green(':)'))
         return callback(null,file);
       });
     });
