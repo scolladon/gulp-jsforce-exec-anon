a4ddb225ddb5271ec6b04dc69c46b6321b7fd154
scolladon <colladonsebastien@gmail.com>
2018-07-26 16:10:49 +0200

    End of authentication switch

--- index.js
+++ index.js
@@ -11,29 +11,55 @@ const PLUGIN_NAME = 'gulp-jsforce-exec-anon';
 let conn = null;
 
 module.exports = options =>{
-  if(conn === null) {
-    // authent
-  }
+  return through.obj((file, enc, cb) => {  
+    authenticate(options)
+    .then(sfConn=> {return new Promise((resolve,reject)=>{
+      sfConn.tooling.executeAnonymous(file.contents, (err, res) => {
+        if(err)return reject(err)
+        else if(!res.success)return reject(res.compileProblem)
+        else{
+          gutil.log(PLUGIN_NAME, path.basename(file.path) + ' successfuly executed anonymously ', gutil.colors.green(':)'))
+          resolve();
+        }
+      });
+    })})
+    .catch((err)=>{
+      return cb(new gutil.PluginError(PLUGIN_NAME,err))
+    })
+    .then(()=> {return cb(null,file)});
+  });
+};
 
-  return through.obj((file, enc, cb) => {
+const authenticate = (options) => {
+  return new Promise((resolve,reject) => {
+    if(conn !== null) {
+      return resolve(conn);
+    }
     
-    jwt.getToken({
+    if(!!options.password) {
+      conn = new jsforce.Connection({
+      loginUrl : options.loginUrl
+      });
+      conn.login(options.username, options.password, (error, userInfo) => {
+        if (error) return reject(error)
+        return resolve(conn);
+      });
+    } else if(!!options.privateKeyPath) {
+      jwt.getToken({
         iss: options.consumerKey,
         sub: options.username,
         aud: options.loginUrl,
         privateKey: fs.readFileSync(options.privateKeyPath).toString('utf8')
-    },(error,token) => {
-      if(error){ return cb(new gutil.PluginError(PLUGIN_NAME,error)); }
-      conn = new jsforce.Connection({
-        instanceUrl : token.instance_url,
-        accessToken : token.access_token
+      },(error,token) => {
+        if(error)return reject(error)
+        conn = new jsforce.Connection({
+          instanceUrl : token.instance_url,
+          accessToken : token.access_token
+        });
+        return resolve(conn);
       });
-      conn.tooling.executeAnonymous(file.contents, (err, res) => {
-        if (err){ return cb(new gutil.PluginError(PLUGIN_NAME,err)); }
-        else if (!res.success) { return cb(new gutil.PluginError(PLUGIN_NAME,res.compileProblem)); }
-        gutil.log(PLUGIN_NAME, path.basename(file.path) + ' successfuly executed anonymously ', gutil.colors.green(':)'))
-        return cb(null,file);
-      });
-    });
-  });
-};
+    } else {
+      return reject('Authentication impossible')
+    }
+  })
+}
